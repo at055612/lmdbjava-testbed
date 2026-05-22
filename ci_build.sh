94f79570ce415cd2ebb450a675edd62c886c771d
at055612 <22818309+at055612@users.noreply.github.com>
2026-05-22 10:54:21 +0100

    Add github CI

---
+++ ci_build.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -e
+
+setup_echo_colours() {
+  # Exit the script on any error
+  set -e
+
+  # shellcheck disable=SC2034
+  if [ "${MONOCHROME}" = true ]; then
+    RED=''
+    GREEN=''
+    YELLOW=''
+    BLUE=''
+    BLUE2=''
+    DGREY=''
+    NC='' # No Colour
+  else 
+    RED='\033[1;31m'
+    GREEN='\033[1;32m'
+    YELLOW='\033[1;33m'
+    BLUE='\033[1;34m'
+    BLUE2='\033[1;34m'
+    DGREY='\e[90m'
+    NC='\033[0m' # No Colour
+  fi
+}
+
+debug_value() {
+  local name="$1"; shift
+  local value="$1"; shift
+  
+  if [ "${IS_DEBUG}" = true ]; then
+    echo -e "${DGREY}DEBUG ${name}: ${value}${NC}"
+  fi
+}
+
+debug() {
+  local str="$1"; shift
+  
+  if [ "${IS_DEBUG}" = true ]; then
+    echo -e "${DGREY}DEBUG ${str}${NC}"
+  fi
+}
+
+main() {
+  IS_DEBUG=false
+  #SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+  setup_echo_colours
+
+  echo -e "${GREEN}Run gradle build${NC}"
+  ./gradlew clean build
+
+  ./buildDocker.sh run
+
+  ./buildDocker.sh run all -i 1000 -t 2
+
+  echo -e "${GREEN}Done${NC}"
+}
+
+main "$@"
